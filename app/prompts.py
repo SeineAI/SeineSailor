245771be300a8d3b8bb41382fe149a24cb55a395
BO WEN <bwen@us.ibm.com>
2024-05-09 15:30:07 -0400

    refactor prompts

--- app/prompts.py
+++ app/prompts.py
@@ -1,5 +1,6 @@
 from inputs import Inputs
 
+
 class Prompts:
     summarize_file_diff = """## GitHub PR Title
 
@@ -102,11 +103,13 @@ $short_summary
 Input: New hunks annotated with line numbers and old hunks (replaced code). Hunks represent incomplete code fragments.
 Additional Context: PR title, description, summaries and comment chains.
 Task: Review new hunks for substantive issues using provided context and respond with comments if necessary.
-Output: Review comments in markdown with exact line number ranges in new hunks. Start and end line numbers must be within the same hunk. For single-line comments, start=end line number. Must use example response format below.
+Output: Review comments in markdown with exact line number ranges in new hunks. Start and end line numbers must be 
+within the same hunk. For single-line comments, start=end line number. Must use example response format below.
 Use fenced code blocks using the relevant language identifier where applicable.
 Don't annotate code snippets with line numbers. Format and indent code correctly.
 Do not use `suggestion` code blocks.
-For fixes, use `diff` code blocks, marking changes with `+` or `-`. The line number range for comments with fix snippets must exactly match the range to replace in the new hunk.
+For fixes, use `diff` code blocks, marking changes with `+` or `-`. The line number range for comments with fix snippets 
+must exactly match the range to replace in the new hunk.
 
 - Do NOT provide general feedback, summaries, explanations of changes, or praises 
   for making good additions. 
