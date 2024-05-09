83e0e941e4f662ccb38a31e757e324cd7162718a
BO WEN <bwen@us.ibm.com>
2024-05-09 18:10:20 -0400

    setup bots api with langchain

--- app/bot.py
+++ app/bot.py
@@ -1,133 +1,97 @@
 import os
-from chatgpt import ChatGPTAPI, ChatGPTError, ChatMessage
-from langchain.community.llms.watsonx_ai import WatsonxAI
-from p_retry import retry
-from options import Options, LLMOptions
 import time
-import datetime
-import json
+from datetime import datetime
+from tenacity import retry, stop_after_attempt, wait_fixed
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.messages import SystemMessage
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ibm import WatsonxLLM
+
+from options import Options, LLMOptions
 
 from logger import setup_logger
 
 logger = setup_logger("bot")
+
 
 class Bot:
     def __init__(self, options: Options, llm_options: LLMOptions):
         self.options = options
         self.llm_options = llm_options
         self.api = None
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        self.system_message = (
+            f"{options.system_message}\n"
+            f"Knowledge cutoff: {llm_options.token_limits.knowledge_cut_off}\n"
+            f"Current date: {current_date}\n\n"
+            f"IMPORTANT: Entire response must be in the language with ISO code: {options.language}"
+        )
+        output_parser = StrOutputParser()
 
         if options.api_type == "openai":
             if os.environ.get("OPENAI_API_KEY"):
-                current_date = datetime.now().strftime("%Y-%m-%d")
-                system_message = f"{options.system_message}\nKnowledge cutoff: {llm_options.token_limits.knowledge_cut_off}\nCurrent date: {current_date}\n\nIMPORTANT: Entire response must be in the language with ISO code: {options.language}"
-
-                self.api = ChatGPTAPI(
-                    api_base_url=options.api_base_url,
-                    system_message=system_message,
-                    api_key=os.environ["OPENAI_API_KEY"],
-                    api_org=os.environ.get("OPENAI_API_ORG", None),
-                    debug=options.debug,
-                    max_model_tokens=llm_options.token_limits.max_tokens,
-                    max_response_tokens=llm_options.token_limits.response_tokens,
-                    completion_params={
-                        "temperature": options.llm_model_temperature,
-                        "model": llm_options.model,
-                    },
+                prompt = ChatPromptTemplate.from_messages([
+                    SystemMessage(content=self.system_message),
+                    HumanMessagePromptTemplate.from_template("{human_input}")
+                ])
+                llm = ChatOpenAI(
+                    openai_api_base=options.api_base_url,
+                    openai_api_key=os.environ["OPENAI_API_KEY"],
+                    openai_organization=os.environ.get("OPENAI_API_ORG", None),
+                    max_tokens=llm_options.token_limits.response_tokens,
+                    temperature=options.llm_model_temperature,
+                    model=llm_options.model
                 )
+                self.api = prompt | llm | output_parser
             else:
                 raise Exception(
                     "Unable to initialize the OpenAI API, the 'OPENAI_API_KEY' environment variable is not available"
                 )
         elif options.api_type == "watsonx":
             if os.environ.get("IBM_CLOUD_API_KEY") and os.environ.get("WATSONX_PROJECT_ID"):
-                self.api = WatsonxAI(
+                if self.llm_options.model == "mistralai/mixtral-8x7b-instruct-v01":
+                    template = f"[INST]{self.system_message}\n\n{{human_input}}[/INST]"
+                elif self.llm_options.model == "meta-llama/llama-3-70b-instruct":
+                    template = (f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n"
+                                f"{self.system_message}<|eot_id|>\n"
+                                f"<|start_header_id|>user<|end_header_id|>\n\n"
+                                f"{{human_input}}<|eot_id|>\n"
+                                f"<|start_header_id|>assistant<|end_header_id|>")
+                else:
+                    template = f"system_prompt: {self.system_message}\n\nuser_input: {{human_input}}"
+                prompt = PromptTemplate.from_template(template)
+                llm = WatsonxLLM(
                     model_id=llm_options.model,
-                    ibm_cloud_api_key=os.environ["IBM_CLOUD_API_KEY"],
+                    url=options.api_base_url,
+                    apikey=os.environ["IBM_CLOUD_API_KEY"],
                     project_id=os.environ["WATSONX_PROJECT_ID"],
-                    model_parameters={
+                    params={
                         "max_new_tokens": llm_options.token_limits.response_tokens,
                         "min_new_tokens": 0,
-                        "stop_sequences": [],
-                        "repetition_penalty": 1,
                         "temperature": options.llm_model_temperature,
                     },
                 )
+                self.api = prompt | llm | output_parser
             else:
                 raise Exception(
-                    "Unable to initialize WatsonX AI, both 'IBM_CLOUD_API_KEY' and 'WATSONX_PROJECT_ID' environment variables are not available"
+                    "Unable to initialize WatsonX AI, both 'IBM_CLOUD_API_KEY' and 'WATSONX_PROJECT_ID' "
+                    "environment variables are not available"
                 )
         else:
-            raise Exception(f"Unsupported API type: {options.api_type}")
+            raise Exception(f"{options.api_type} API is not supported")
 
-    def format_prompt(self, user_message: str) -> str:
-        if self.options.api_type == "watsonx":
-            current_date = datetime.now().strftime("%Y-%m-%d")
-            system_prompt = f"{self.options.system_message}\nKnowledge cutoff: {self.llm_options.token_limits.knowledge_cut_off}\nCurrent date: {current_date}\n\nIMPORTANT: Entire response must be in the language with ISO code: {self.options.language}"
-            if self.llm_options.model == "mistralai/mixtral-8x7b-instruct-v01":
-                return f"[INST]{system_prompt}\n\n{user_message}[/INST]"
-            elif self.llm_options.model == "meta-llama/llama-3-70b-instruct":
-                return f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{system_prompt}<|eot_id|>\n<|start_header_id|>user<|end_header_id|>\n\n{user_message}<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>"
-
-        return user_message
-
-    async def chat(self, message: str, ids: dict) -> tuple[str, dict]:
-        try:
-            return await self._chat(message, ids)
-        except ChatGPTError as e:
-            logger.warning(f"Failed to chat: {e}, backtrace: {e.stack}")
-            return "", {}
-
-    async def _chat(self, message: str, ids: dict) -> tuple[str, dict]:
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
+    async def chat(self, message: str):
         start = time.time()
-
-        if not message:
-            return "", {}
-
-        response = None
-
-        if self.api is not None:
-            if self.options.api_type == "openai":
-                opts = {"timeout_ms": self.options.llm_timeout_ms}
-                if ids.get("parent_message_id"):
-                    opts["parent_message_id"] = ids["parent_message_id"]
-                try:
-                    response = await retry(
-                        lambda: self.api.send_message(message, opts),
-                        retries=self.options.llm_retries,
-                    )
-                except ChatGPTError as e:
-                    logger.info(
-                        f"response: {response}, failed to send message to OpenAI: {e}, backtrace: {e.stack}"
-                    )
-            elif self.options.api_type == "watsonx":
-                formatted_prompt = self.format_prompt(message)
-                try:
-                    response = await retry(
-                        lambda: self.api.invoke(formatted_prompt),
-                        retries=self.options.llm_retries,
-                    )
-                except Exception as e:
-                    logger.info(
-                        f"response: {response}, failed to send message to WatsonX AI: {e}, backtrace: {e.stack}"
-                    )
-
-            end = time.time()
-            logger.info(f"response: {json.dumps(response)}")
-            logger.info(
-                f"{self.options.api_type} {'sendMessage' if self.options.api_type == 'openai' else 'invoke'} (including retries) response time: {end - start} ms"
-            )
-        else:
-            raise Exception(f"{self.options.api_type} API is not initialized")
-
         response_text = ""
-        if response is not None:
-            if self.options.api_type == "openai":
-                response_text = response.text
-            elif self.options.api_type == "watsonx":
-                response_text = response
-        else:
-            logger.warning(f"{self.options.api_type} response is null")
+        try:
+            response_text = await self.api.ainvoke({"human_input": message})
+        except Exception as e:
+            logger.error(f"Failed to send message to {self.options.api_type}: {e}")
+
+        end = time.time()
+        logger.info(f"Response: {response_text}\n\nResponse time: {end - start} ms")
 
         if response_text.startswith("with "):
             response_text = response_text[5:]
@@ -135,9 +99,24 @@ class Bot:
         if self.options.debug:
             logger.info(f"{self.options.api_type} responses: {response_text}")
 
-        new_ids = {}
-        if self.options.api_type == "openai":
-            new_ids["parent_message_id"] = response.id
-            new_ids["conversation_id"] = response.conversation_id
+        return response_text
 
-        return response_text, new_ids
+
+if __name__ == "__main__":
+    # test
+    import asyncio
+
+    openai_option = Options(True, False, False,
+                            llm_light_model="gpt-3.5-turbo", api_type="openai",
+                            api_base_url="https://api.openai.com/v1")
+    openai_bot = Bot(openai_option, LLMOptions("gpt-3.5-turbo"))
+    watsonx_option = Options(True, False, False)
+    watsonx_bot = Bot(watsonx_option, LLMOptions("mistralai/mixtral-8x7b-instruct-v01"))
+
+
+    async def main():
+        response = await openai_bot.chat("This is a test. Please generate 'Hello World' and today's date.")
+        print(response)
+
+
+    asyncio.run(main())
