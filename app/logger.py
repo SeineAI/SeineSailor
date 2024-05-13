c9eb6a13e428c2e3dbe2096a5255886b116bbd0c
BO WEN <bwen@us.ibm.com>
2024-05-12 22:03:19 -0400

    The logger level need to be set inside the function. Otherwise it can't be updated by the ENV var.

--- app/logger.py
+++ app/logger.py
@@ -2,12 +2,16 @@ import logging
 import os
 
 
-def setup_logger(name, level=int(os.environ.get("SEINE_SAILOR_LOG_LEVEL", logging.INFO))):
+def setup_logger(name, level=None):
+    if level:
+        level_value = int(level)
+    else:
+        level_value = int(os.environ.get("SEINE_SAILOR_LOG_LEVEL", logging.INFO))
     logger = logging.getLogger(name)
-    logger.setLevel(level)
+    logger.setLevel(level_value)
 
     handler = logging.StreamHandler()
-    handler.setLevel(level)
+    handler.setLevel(level_value)
 
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
